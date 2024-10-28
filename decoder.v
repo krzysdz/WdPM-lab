d19ed701cc655dde3db0800cf57d30324c6438f6
krzysdz <krzysdz@users.noreply.github.com>
2024-10-28 15:00:47 +0100

    Comment the decoder logic

--- decoder.v
+++ decoder.v
@@ -10,10 +10,16 @@ module decoder (
     output ce_a,
     output ce_cy
 );
+    // ALU opcode, top bit distinguishes only between ST and NOP,
+    // for which writes to A and CY are disabled.
     assign op = inst[4:2];
+    // Register address
     assign a = inst[1:0];
+    // Write to registers only with ST
     assign ce_reg = inst[5:2] == 4'b0111;
+    // Write to accumulator for all operations except ST (0111) and NOP (1111)
     assign ce_a = inst[4:2] != 3'b111;
+    // Update carry flag for all operations except LD (0110, 1110), ST and NOP
     assign ce_cy = inst[4:3] != 2'b11;
 endmodule
 `endif
